76e0dac6ef959fdd7b3eab65f7181c44ca4aa420
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-10-13 05:10:17 +0100

    Finished about section

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -19,17 +19,23 @@ function Dashboard({ token }) {
         <Link to='/login' onClick={handleLogOut}>Log out</Link>
 
         <div className="info-cards-grid">
-          <div className="info-card user-details">
-          <img src="https://previews.123rf.com/images/mimagephotography/mimagephotography1601/mimagephotography160100473/51497473-closeup-portrait-of-smiling-young-black-woman-against-white-background.jpg" alt="Hero image" className="profile-photo"/>
-            <div className="user-info">
-              <p className="username">{token.user.user_metadata.full_name}</p>
-              <p className="nickname">@{token.user.user_metadata.user_name}</p>
-              <button className="book-btn">Book now</button>
-            </div>
+          <div className="info-card">
+          <div className="user-details">
+            <img src="https://previews.123rf.com/images/mimagephotography/mimagephotography1601/mimagephotography160100473/51497473-closeup-portrait-of-smiling-young-black-woman-against-white-background.jpg" alt="Hero image" className="profile-photo"/>
+              <div className="user-info">
+                <p className="username">{token.user.user_metadata.full_name}</p>
+                <p className="nickname">@{token.user.user_metadata.user_name}</p>
+                <button className="book-btn">Book now</button>
+              </div>
+          </div>
+            <h5 className="about-header">About me</h5>
+            <p>I am a photographer who likes to have fun ya know? I've been taking pictures for forever now and I hope to keep taking more!!!</p>
           </div>
 
           <div className="info-card">
-            Featured Content
+            <h5 className="featured-header">Featured Content</h5>
+            <p>Upgrade your account in order to upload content for potential customers to see what thee can do</p>
+            <button>Uprade account</button>
           </div>
 
           <div className="info-card">
